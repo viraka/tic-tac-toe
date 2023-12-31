3c4b2ad1737a7fc0317acb5ece561a385c5aa2bb
BlazeSN860 <virajprabhup@gmail.com>
2023-12-31 10:41:35 +0530

    feat: new PWA feature added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Tic Tac Toe",
+        short_name: "Tic Tac Toe",
+        description: "Simple app to play tic tac toe.",
+        icons: [
+          {
+            src: '/tic-tac-toe.svg',
+            sizes: '800x800',
+            type: 'image/svg+xml'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ]
 })
